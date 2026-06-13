5102e3aac653f14e91139445458f0f834565f7a4
Deepak-Sharma-141 <dsharma19963006@gmail.com>
2026-06-13 22:22:12 +0530

    Creating the Entity classes Part 2

---
+++ razorpay/src/main/java/com/deepakproject/razorpay/common/enums/RefundStatus.java
@@ -0,0 +1,8 @@
+package com.deepakproject.razorpay.common.enums;
+
+public enum RefundStatus {
+    PENDING,
+    PROCESSING,
+    PROCESSED,
+    FAILED
+}
---
+++ razorpay/src/main/java/com/deepakproject/razorpay/common/enums/WebhookEventStatus.java
@@ -0,0 +1,8 @@
+package com.deepakproject.razorpay.common.enums;
+
+public enum WebhookEventStatus {
+    PENDING,
+    DELIVERED,
+    FAILED,
+    DEAD
+}
